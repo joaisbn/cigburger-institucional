18a81609144a741b07728d2310b0fd677f61715c
Joais Buriti <joais.nascimento@uerj.br>
2024-07-19 15:15:37 -0300

    paginas produtos e onde estamos

--- app/Views/where_we_are.php
+++ app/Views/where_we_are.php
@@ -27,8 +27,42 @@
         </div>
     </nav>
 
-    <!-- conteúdo -->
-    <h1>Onde estamos?</h1>
+    <!-- main -->
+    <section class="container product-box py-5">
+        <div class="row">
+            <div class="col-5 text-center">
+                <img class="img-fluid" src="<?= base_url('assets/images/room.jpg') ?>" alt="Our room">
+            </div>
+            <div class="col-6">
+                <p class="where-we-are-title mb-0">CIGBurger Paris</p>
+                <p class="where-we-are-subtitle">Rua des hamburgers, 123 Paris</p>
+                <p class="mb-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati distinctio reprehenderit optio? Praesentium odio dicta optio explicabo, nisi corrupti eveniet repellat. Vel pariatur facere laboriosam modi iste corporis repellendus nulla.</p>
+
+                <div class="d-flex align-items-center mb-3">
+                    <img src="<?= base_url('assets/images/icon_phone.png') ?>" alt="Phone">
+                    <p class="where-we-are-title ms-3 mb-0">
+                        <a class="nav-link" href="tel:+552122345678">+55 21 2234-5678</a>
+                    </p>
+                </div>
+
+                <div class="d-flex align-items-center mb-3">
+                    <img src="<?= base_url('assets/images/icon_email.png') ?>" alt="E-mail">
+                    <p class="where-we-are-title ms-3 mb-0">
+                        <a class="nav-link" href="mailto:contato@cigburger.com">contato@cigburger.com</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- map -->
+    <section class="container product-box py-5">
+        <div class="row">
+            <div class="col text-center">
+                <img src="<?= base_url('assets/images/map.jpg') ?>" alt="Map">
+            </div>
+        </div>
+    </section>
 
     <!-- social networks -->
      <footer class="container-fluid mt-5">
